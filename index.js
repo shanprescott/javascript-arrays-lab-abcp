3e1552a32d30adca86506ee16b8bdef9cc6a4261
Shannon Prescott <shan.prescott@outlook.com>
2018-01-19 17:12:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,14 @@ function removeLastKitten() {
   return kittens.slice(0, kittens.length-1)
   
 }
+removeLastKitten();
 
+kittens = ['Milo', 'Otis', 'Garfield']
+
+function removeFirstKitten() {
+  return kittens.slice(0, kittens.length-1)
+  
+}
 
 
 
