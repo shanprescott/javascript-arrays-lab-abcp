8599b90eb59d63b830b9ef708fd7922c826537ce
Shannon Prescott <shan.prescott@outlook.com>
2018-01-19 16:31:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 
 function  destructivelyAppendKitten(name) {
-  kittens.shift('Ralph')
+  kittens.push('Ralph')
   return kittens
   
 }
